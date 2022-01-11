309d16dad22b39f42de0c8e2f5a451cf4b7847f2
Michael Woodyard <mbwoodyard@hotmail.com>
2022-01-11 15:13:10 -0500

    • Added create_app() to __init__.py.

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,10 @@
 # Turns website folder into a package, allows importing.
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__) # Represents the name of the file.
+    app.config['SECRET_KEY'] = ''
+
+    return app
+
+    # TIME: 10:36
